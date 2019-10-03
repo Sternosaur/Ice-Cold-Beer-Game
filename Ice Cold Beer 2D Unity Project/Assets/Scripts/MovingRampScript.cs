df4b200cbd6d06636549be38bad69c1d4da87456
Sternosaur <55261820+Sternosaur@users.noreply.github.com>
2019-10-03 18:11:03 -0500

    added a script and such

---
+++ Ice Cold Beer 2D Unity Project/Assets/Scripts/MovingRampScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRampScript : MonoBehaviour
+{
+    private float moveSpeed = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Test hello world.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if the player presses up arrow, we move the square up.
+        //if the player presses the down arrow, we move the square down.
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, moveSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -moveSpeed, 0);
+        }
+
+    }
+}
