e0be4be6fb79eb208d70d652c54e7511ce748417
Sternosaur <Sternosaur@gmail.com>
2019-10-24 17:08:52 -0600

    added a new script and some text

---
+++ Ice Cold Beer 2D Unity Project/Assets/Scripts/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Detect when the ball touches the goal, and then win the level.
+/// </summary>
+
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touched trigger.");
+        if (collision.tag == "Player")
+        {
+            Debug.Log("The layer has entered the goal");
+        }
+    }
+}
